72b3c6da4f59687509c54e4169133370275cd240
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 17:04:07 +0100

    Solved OddEven exercise

---
+++ week-01/day-4/src/OddEven.java
@@ -0,0 +1,12 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int number = 8;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}
