70b17f47895ffb4965942daa1d61270e0ab42630
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 12:40:41 +0100

    HelloOthers program created

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gyuri!");
+        System.out.println("Hello, Kristóf!");
+        System.out.println("Hello, Dani!");
+    }
+}
