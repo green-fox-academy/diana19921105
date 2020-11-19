4480cb8fabacbb36554b4a2b3a2518ab8d21de33
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 13:06:01 +0100

    IntroduceYourself program created

---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,11 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        String name = "Diána Szántó";
+        int age = 28;
+        double height = 1.61;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
