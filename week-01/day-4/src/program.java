7d5e137e149e3ed89a72743ce93ffad17d02bb81
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 10:56:47 +0100

    Week 1 day 4 matreview in progress: types and operators

---
+++ week-01/day-4/src/program.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+        //bekérünk két számot, és az összegüket
+        // számokat tárolni : változókban
+        // számok :
+        //egész:
+        int num = 6;
+
+        //valós számok
+        float num2 = 1.5F;
+        double num3;
+
+        int sum = num2 + num3;
+
+        Scanner scanner = new Scanner(in);
+
+        int a =scanner.nextInt();
+        int b =scanner.nextInt();
+        int c =scanner.nextInt();
+    }
+}
