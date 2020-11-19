0d9ab82250e69407fd2097d8db7c7cbeebbb6ba1
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 16:00:14 +0100

    Solved MileToKmConverter exercise

---
+++ week-01/day-4/src/MileToKmConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MileToKmConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of miles!");
+        double userInput = scanner.nextDouble();
+
+        double kilometers = userInput * 1.609344;
+
+        System.out.println(kilometers);
+    }
+}
