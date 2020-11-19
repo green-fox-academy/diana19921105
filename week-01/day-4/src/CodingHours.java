41a91e1acb50b3b39e366b34ba65f26cfaa13bfc
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 20:58:17 +0100

    Fix the CodingHours program

--- week-01/day-4/src/CodingHours.java
+++ week-01/day-4/src/CodingHours.java
@@ -4,17 +4,14 @@ public class CodingHours {
         int semesterPeriodInWeeks = 17;
         int semesterPeriodWorkDays = semesterPeriodInWeeks * 5;
 
-        //System.out.println(semesterPeriodWorkDays);
-
         int codingHoursPerSemester = dailyCodingHours * semesterPeriodWorkDays;
 
         System.out.println(codingHoursPerSemester);
 
         int averageWorkHours = 52;
         int totalWorkingHours = semesterPeriodInWeeks * averageWorkHours;
-        double percentageOfCodingHours = (double) codingHoursPerSemester / averageWorkHours;
+        double percentageOfCodingHours = ((double) codingHoursPerSemester / totalWorkingHours) * 100;
 
         System.out.println(percentageOfCodingHours);
-
     }
 }
