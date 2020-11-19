826a969fc70383a82fd14d87e31219665bc75836
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 13:26:37 +0100

    Solved TwoNumbers exercise

---
+++ week-01/day-4/src/TwoNumbers.java
@@ -0,0 +1,20 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 22;
+        int num2 = 13;
+
+        int amount = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double doubleRatio = (double) num1 / num2;
+        int ratio = num1 / num2;
+        int remainResult = num1 % num2;
+
+        System.out.println(amount);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(doubleRatio);
+        System.out.println(ratio);
+        System.out.println(remainResult);
+    }
+}
