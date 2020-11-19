50c0c48ae1b0745a06d765e4d5280ee5321df0fd
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 13:57:07 +0100

    Solved Swap exercise

---
+++ week-01/day-4/src/Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        a = 123;
+        b = 526;
+
+        a = 526;
+        b = 123;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
