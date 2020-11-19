129a3668ffc77d3eb6d3f78c4a7eb175cd5f208f
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 17:08:55 +0100

    Solved OneTwoALot exercise

---
+++ week-01/day-4/src/OneTwoALot.java
@@ -0,0 +1,18 @@
+public class OneTwoALot {
+    public static void main(String[] args) {
+        int number = 200;
+
+        if (number == 0) {
+            System.out.println("Not enough");
+        }
+        else if (number == 1) {
+            System.out.println("One");
+        }
+        else if (number == 2) {
+            System.out.println("Two");
+        }
+        else {
+            System.out.println("A lot");
+        }
+    }
+}
