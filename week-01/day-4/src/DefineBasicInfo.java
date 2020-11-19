521ad98f421e9f92646d66661e4c54ac4ee6382b
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-19 14:02:26 +0100

    Solved DefineBasicInfo exercise

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+
+        String name = "Diána Szántó";
+        int age = 28;
+        double height = 1.61;
+        boolean isMarried = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(isMarried);
+    }
+}
