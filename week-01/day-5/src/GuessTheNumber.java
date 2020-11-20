e93416cb9ba64599ec1685f73be2133444b42b4b
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-20 13:47:25 +0100

    Solved GuessTheNumber exercise

---
+++ week-01/day-5/src/GuessTheNumber.java
@@ -0,0 +1,35 @@
+// Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stored number is lower
+// You found the number: 8
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        int storedNumber = 8;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which number did I think?");
+        int guessNumber = 0;
+
+        do {
+            guessNumber = scanner.nextInt();
+
+            if (storedNumber > guessNumber) {
+                System.out.println("The stored number is higher");
+
+            } else if (storedNumber < guessNumber) {
+                System.out.println("The stored number is lower");
+
+            } else {
+                System.out.println("You found the number: " + guessNumber);
+            }
+        }
+        while (guessNumber != storedNumber);
+    }
+
+}
+
