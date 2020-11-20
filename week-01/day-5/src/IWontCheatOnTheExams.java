c7b4cace5a4ec938d3372f23a284d38137eaa01b
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-20 11:43:42 +0100

    Solved IWontCheat... exercise

---
+++ week-01/day-5/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+public class IWontCheatOnTheExams {
+    public static void main (String[] args) {
+        int i = 0;
+        while (i < 100){
+            System.out.println("I won't cheat on the exam!");
+            i++;
+        }
+    }
+}
