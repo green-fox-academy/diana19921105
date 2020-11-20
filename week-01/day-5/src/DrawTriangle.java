65bb31b941404ec67fd66e74614b428152b63b4f
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-20 15:19:09 +0100

    Solved drawtriangle exercise

---
+++ week-01/day-5/src/DrawTriangle.java
@@ -0,0 +1,21 @@
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
