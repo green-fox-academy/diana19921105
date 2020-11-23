ed82b1e22112e5aa3148b071ed9a4ece7ab8944c
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-23 12:19:30 +0100

    Solved Sumelements exercise

---
+++ week-02/day-01/src/SumElements.java
@@ -0,0 +1,10 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int[] numbers = {54, 23, 66, 12};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = numbers[1] + numbers[2];
+        }
+        System.out.println(sum);
+    }
+}
