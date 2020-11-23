a200135ef144e23287549eedd64771a8d143763a
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-23 16:31:29 +0100

    Solved Swap elements exercise

---
+++ week-02/day-01/src/SwapElements.java
@@ -0,0 +1,19 @@
+// - Create an array variable named `orders`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `orders`
+
+import java.util.Arrays;
+
+public class SwapElements {
+    public static void main(String[] args) {
+        String[] orders = {"first", "second", "third"};
+        String temp = "";
+
+        for (int i = 0; i < orders.length; i++) {
+            temp = orders[0];
+            orders[0] = orders[2];
+            orders[2] = temp;
+        }
+        System.out.println(Arrays.toString(orders));
+    }
+}
