d05c9a8c69210b7795f9a42d148efaa1f2c8b090
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-23 16:14:29 +0100

    Solved Factorial exercise

---
+++ week-02/day-01/src/Factorial.java
@@ -0,0 +1,20 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+public class Factorial {
+
+    public static long factorial(int number) {
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
+}
