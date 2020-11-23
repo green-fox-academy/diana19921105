8cc48f06725eae3f926f452149f8871d88070056
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-23 12:31:33 +0100

    Solved Incrementelement exercise

---
+++ week-02/day-01/src/IncrementElement.java
@@ -0,0 +1,11 @@
+public class IncrementElement {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 2) {
+                numbers[i]++;
+            }
+        }
+        System.out.println(numbers[2]);
+    }
+}
