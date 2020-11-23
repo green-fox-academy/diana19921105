0effa5eff7e424669c5844f3b29b4b6b3b9a127a
Diana Szanto <szanto.diana.1992@gmail.com>
2020-11-23 12:36:49 +0100

    Modify the program with the instructions

--- week-02/day-01/src/ChangeElement.java
+++ week-02/day-01/src/ChangeElement.java
@@ -1,3 +1,8 @@
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4
+// - Print the fourth element
+
 public class ChangeElement {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 8, 5, 6};
