04eb83b3002c53025eb46a7ffd31082a1127f84c
Diana Szanto <szanto.diana.1992@gmail.com>
2020-12-01 20:21:01 +0100

    Fix the code

--- week-03/day-02/src/CopyFile.java
+++ week-03/day-02/src/CopyFile.java
@@ -3,35 +3,33 @@
 // It should return a boolean that shows if the copy was successful
 //
 
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
 public class CopyFile {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println(copyFile("original.txt", "dest.txt"));
     }
 
-    private static boolean copyFile(String original, String destination) throws IOException {
-        Path filePath = Paths.get(original);
-        Path copyFile = Paths.get(destination);
+    private static boolean copyFile(String original, String destination) {
 
         try {
-        List<String> lines = Files.readAllLines(filePath);
+            Path filePath = Paths.get(original);
+            Path copyFile = Paths.get(destination);
+            List<String> lines = Files.readAllLines(filePath);
             StringBuilder copy = new StringBuilder();
             for (String line : lines) {
                 copy.append(line);
                 copy.append("\n");
             }
             Files.write(copyFile, Collections.singleton(copy));
-        } catch (NoSuchFileException ignored) {
+            return true;
+        } catch (Exception exception) {
+            return false;
         }
-
-        return false;
     }
 }
