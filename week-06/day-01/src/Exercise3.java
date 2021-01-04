b5c76acb86d3cbe69aa318705c3289c1df2ee3d2
Diana Szanto <szanto.diana.1992@gmail.com>
2021-01-04 12:29:01 +0100

    Improve the code

--- week-06/day-01/src/Exercise3.java
+++ week-06/day-01/src/Exercise3.java
@@ -5,6 +5,10 @@ public class Exercise3 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
 
+        greaterThenSquare(numbers);
+    }
+
+    private static void greaterThenSquare(List<Integer> numbers) {
         numbers.stream()
                 .map(x -> x * x)
                 .filter(x -> x > 20)
