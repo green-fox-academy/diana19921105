eaeda67562b80dafd87d79bf077cb9ae322e67fd
Diana Szanto <szanto.diana.1992@gmail.com>
2021-01-06 14:11:45 +0100

    Solved the Hello User REST App

--- week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting(12L, "hjhhj");
+    public Greeting greeting(@RequestParam String name) {
+        return new Greeting(1L, "Hello " + name);
     }
 }
