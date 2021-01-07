743374637fbfc8e9d5466253eec453f724e76281
Diana Szanto <szanto.diana.1992@gmail.com>
2021-01-07 15:12:40 +0100

    Add currency

--- week-06/day-04/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
+++ week-06/day-04/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
@@ -4,16 +4,24 @@ import com.greenfox.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BankController {
 
     @GetMapping("/show")
-    public String bankAccount(Model model) {
-        BankAccount simba = new BankAccount("Simba", 2000L, "lion");
+    public String show(Model model) {
+        BankAccount simba = new BankAccount("Simba", (double) 2000L, "lion", "Zebra");
         model.addAttribute("name", simba.getName());
-        model.addAttribute("balance", simba.getBalance());
+        model.addAttribute("balance", simba.getBalanceFormatted());
         model.addAttribute("type", simba.getAnimalType());
+        model.addAttribute("currency", simba.getCurrency());
         return "index";
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public String html() {
+        return "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
     }
 }
---
+++ week-06/day-04/src/main/java/com/greenfox/bankofsimba/model/BankAccount.java
@@ -0,0 +1,52 @@
+package com.greenfox.bankofsimba.model;
+
+import java.util.Objects;
+
+public class BankAccount {
+    private String name;
+    private Double balance;
+    private String animalType;
+    private String currency;
+
+    public BankAccount(String name, Double balance, String animalType, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount that = (BankAccount) o;
+        return getName().equals(that.getName()) &&
+                Objects.equals(getBalance(), that.getBalance()) &&
+                getAnimalType().equals(that.getAnimalType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getBalance(), getAnimalType());
+    }
+
+    public String getBalanceFormatted() {
+        return String.format("%.2f", balance);
+    }
+}
