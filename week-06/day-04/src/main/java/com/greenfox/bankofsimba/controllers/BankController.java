168dccdd32c1224f0146e4d1c1f858848a6fb9f5
Diana Szanto <szanto.diana.1992@gmail.com>
2021-01-07 16:10:00 +0100

    Solved the Filling multiple Accounts exercise

--- week-06/day-04/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
+++ week-06/day-04/src/main/java/com/greenfox/bankofsimba/controllers/BankController.java
@@ -5,8 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class BankController {
+
+    private List<BankAccount> bankAccountList;
+
+    public BankController() {
+        bankAccountList = new ArrayList<>();
+        bankAccountList.add(new BankAccount("Nala", (double) 1500L, "lion"));
+        bankAccountList.add(new BankAccount("Zazu", (double) 1L, "bird"));
+        bankAccountList.add(new BankAccount("Mufasa", (double) 10000L, "lion"));
+        bankAccountList.add(new BankAccount("Rafiki", (double) 2500L, "gorilla"));
+        bankAccountList.add(new BankAccount("Timon", (double) 6500L, "meerkat"));
+    }
 
     @GetMapping("/show")
     public String show(Model model) {
@@ -17,10 +32,17 @@ public class BankController {
         return "index";
     }
 
-    @GetMapping("ception")
+    @GetMapping("/ception")
     public String html(Model model) {
         String text = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
         model.addAttribute("text", text);
         return "htmlception";
     }
+
+    @GetMapping("/accounts")
+    public String getMultipleAccounts(Model model) {
+        model.addAttribute("accountList", bankAccountList);
+        return "multipleaccounts";
+    }
+
 }
