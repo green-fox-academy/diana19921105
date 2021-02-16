73b7342cd30df50e7a06c6573d8595b0670b3496
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-16 17:08:01 +0100

    work in progress

---
+++ week-11/src/main/java/com/greenfox/springadvanced/UserRepository.java
@@ -0,0 +1,6 @@
+package com.greenfox.springadvanced;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<> {
+}
--- week-11/src/main/java/com/greenfox/springadvanced/controller/MainController.java
+++ week-11/src/main/java/com/greenfox/springadvanced/controller/MainController.java
@@ -2,12 +2,23 @@ package com.greenfox.springadvanced.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 
-    @GetMapping("/lofaszka")
-    public String helloWorld() {
-        return "Hello World";
+    @GetMapping("/home")
+    public String home() {
+        return "<h1>Hello World!<h1>";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Hello User!<h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h1>Hello Admin!<h1>";
     }
 }
